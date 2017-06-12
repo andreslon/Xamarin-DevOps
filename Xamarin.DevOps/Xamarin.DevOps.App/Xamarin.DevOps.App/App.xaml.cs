0dd80a91f5a01638916689ec3d4a862d4437aab8
Andrés Londoño <andreslon@outlook.com>
2017-06-11 23:44:24 -0500

    Instalacion de mobile center

--- Xamarin.DevOps/Xamarin.DevOps.App/Xamarin.DevOps.App/App.xaml.cs
+++ Xamarin.DevOps/Xamarin.DevOps.App/Xamarin.DevOps.App/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 using Xamarin.Forms;
 
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
 namespace Xamarin.DevOps.App
 {
     public partial class App : Application
@@ -19,6 +23,7 @@ namespace Xamarin.DevOps.App
         protected override void OnStart()
         {
             // Handle when your app starts
+            MobileCenter.Start("android=9c5a4480-99ef-4944-861f-7bdae76174d3", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
